459337fb6a819955af30dd1eff0e4a0e2efd5746
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-09-27 19:28:10 -0500

    Added incremental for loop to generate buttons for each answer option. For some reason the options display above the question. Troubleshooting now.

--- script.js
+++ script.js
@@ -4,28 +4,40 @@ var secondsLeft = 60;
 var mainEl = document.querySelector('#main');
 var startButton = document.querySelector('#start');
 //Each question is an object consisting of a question, 4 answers and a correct answer value.
-var question1 = { question: "placeholder 1", choices:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
-var question2 = { question: "placeholder 2", choices:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
-var question3 = { question: "placeholder 3", choices:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
-var question4 = { question: "placeholder 4", choices:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
-var question5 = { question: "placeholder 5", choices:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
+var question1 = { question: "placeholder 1", options:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
+var question2 = { question: "placeholder 2", options:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
+var question3 = { question: "placeholder 3", options:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
+var question4 = { question: "placeholder 4", options:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
+var question5 = { question: "placeholder 5", options:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
 //Questions are randomly selected using the following array and function
 var questionSet = [question1, question2, question3, question4, question5]
 var currentQuestion = questionSet[Math.floor(Math.random() * questionSet.length)];
-console.log (currentQuestion.choices)
+console.log (currentQuestion.options)
 // //This function is supposed to overwrite "display: none" on the style of the choices container with the radio buttons. Not working right now.
 // function setChoices() {
 //     var choices = document.getElementById("choices");
 //     choices.style = "display: block";
 // }
-
 function setQuestion() {
-var onScreenQuestion = document.createElement('p');
-onScreenQuestion.textContent = currentQuestion.question;
-document.body.appendChild(onScreenQuestion);
-console.log(onScreenQuestion)
-   
+    var onScreenQuestion = document.createElement('p');
+    onScreenQuestion.textContent = currentQuestion.question;
+    document.body.appendChild(onScreenQuestion);
+    console.log(onScreenQuestion)
+       
+    }
+    
+
+function setOptions() {
+    for(var i=0; i<currentQuestion.options.length; i++) {
+        optionButton = document.createElement('button');
+        document.querySelector("#options").appendChild(optionButton);
+        optionButton.setAttribute("id",currentQuestion.options[i]);
+        optionButton.textContent = currentQuestion.options[i];
+        console.log(optionButton);
+    };
 }
+
+
 
 
 
@@ -34,8 +46,8 @@ function startGame() {
     setTime();
     console.log("game start");
     setQuestion();
-    startButton.remove;
-    setChoices;
+    startButton.remove();
+    setOptions();
   
 }
 
