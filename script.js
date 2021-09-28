c9f68d727468e67adabda08f72263d26afc2bbc1
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-09-27 21:21:12 -0500

    Created function to check answers. Event listener on option button saves choice as new variable and compares to correct answer key from question object. Created functions to send correct/incorrect message anmd incorporated into cheack answer logic.

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ var secondsLeft = 60;
 var mainEl = document.querySelector('#main');
 var startButton = document.querySelector('#start');
 //Each question is an object consisting of a question, 4 answers and a correct answer value.
-var question1 = { question: "placeholder 1", options:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
-var question2 = { question: "placeholder 2", options:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
-var question3 = { question: "placeholder 3", options:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
-var question4 = { question: "placeholder 4", options:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
-var question5 = { question: "placeholder 5", options:["A: placeholder", "B: placeholder", "C: placeholder", "D: placeholder"], answer: "A" }
+var question1 = { question: "placeholder 1", options:["A", "B", "C", "D"], answer: "A" }
+var question2 = { question: "placeholder 2", options:["A", "B", "C", "D"], answer: "A" }
+var question3 = { question: "placeholder 3", options:["A", "B", "C", "D"], answer: "A" }
+var question4 = { question: "placeholder 4", options:["A", "B", "C", "D"], answer: "A" }
+var question5 = { question: "placeholder 5", options:["A", "B", "C", "D"], answer: "A" }
 //Questions are randomly selected using the following array and function
 var questionSet = [question1, question2, question3, question4, question5]
 var currentQuestion = questionSet[Math.floor(Math.random() * questionSet.length)];
-console.log (currentQuestion.options)
+var userChoice = '';
+// console.log (currentQuestion.options)
 // //This function is supposed to overwrite "display: none" on the style of the choices container with the radio buttons. Not working right now.
 // function setChoices() {
 //     var choices = document.getElementById("choices");
 //     choices.style = "display: block";
 // }
+//Creates a new p element and assigns the content of the current question to it before appending it to make it visible
 function setQuestion() {
     var onScreenQuestion = document.createElement('p');
     onScreenQuestion.textContent = currentQuestion.question;
@@ -25,8 +27,43 @@ function setQuestion() {
     console.log(onScreenQuestion)
        
     }
-    
+   //These functions send messages to user and are called based on game results
+function sendLoserMessage(){
+    var loserMessage = document.createElement("h1");
+    loserMessage.textContent = "LOSER!!!"
+    mainEl.appendChild(loserMessage);
+ }
+ function sendWinnerMessage(){
+     var winnerMessage = document.createElement('h1');
+     winnerMessage.textContent = "You win!"
+     mainEl.appendChild(winnerMessage);
+ }
+ 
+ function sendCorrectAnswerMessage(){
+     var correctAnswerMessage = document.createElement('h1');
+     correctAnswerMessage.textContent = "Correct Answer!"
+     mainEl.appendChild(correctAnswerMessage);
+ }
+ 
+ function sendIncorrectAnswerMessage(){
+     var incorrectAnswerMessage = document.createElement('h1');
+     incorrectAnswerMessage.textContent = "Incorrect Answer!"
+     mainEl.appendChild(incorrectAnswerMessage);
+ }
 
+
+    function checkAnswer() {
+        if (userChoice == currentQuestion.answer) {
+            console.log('correct');
+            sendCorrectAnswerMessage();
+        }
+        else {
+            sendIncorrectAnswerMessage();
+        }
+    }
+
+    
+//This for loop generates a button for each option within the current question
 function setOptions() {
     for(var i=0; i<currentQuestion.options.length; i++) {
         optionButton = document.createElement('button');
@@ -35,8 +72,28 @@ function setOptions() {
         optionButton.textContent = currentQuestion.options[i];
         console.log(optionButton);
     };
+    var optionA = document.getElementById("A");
+    var optionB = document.getElementById("B")
+    var optionC = document.getElementById("C")
+    var optionD = document.getElementById("D")
+//These event listeners will set userChoice equal to the value of the option button clicked and run the checkAnswer function, which will compare to the correct answer stored in the question object.
+    optionA.addEventListener("click", function(){
+        userChoice='A';
+        checkAnswer();
+    });
+    optionB.addEventListener("click", function(){
+        userChoice='B';
+        checkAnswer();
+    });
+    optionC.addEventListener("click", function(){
+        userChoice='C';
+        checkAnswer();
+    });
+    optionD.addEventListener("click", function(){
+        userChoice='D';
+        checkAnswer();
+    });
 }
-
 
 
 
@@ -68,11 +125,4 @@ function setTime() {
     }, 1000); //timer interval in ms
 }
 
-// setTime()
-// console.log(secondsLeft)
 
-function sendMessage(){
-   var loserMessage = document.createElement("h1");
-   loserMessage.textContent = "LOSER!!!"
-   mainEl.appendChild(loserMessage);
-}
